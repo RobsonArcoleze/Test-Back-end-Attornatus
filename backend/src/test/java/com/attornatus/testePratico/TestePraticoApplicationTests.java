0b0d3173c93117a729f4f5dc8842c9ead5c5a1ed
RobsonDev <robson.arcoleze29@gmail.com>
2023-01-03 15:20:53 -0300

    Criado projeto

---
+++ backend/src/test/java/com/attornatus/testePratico/TestePraticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.testePratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
