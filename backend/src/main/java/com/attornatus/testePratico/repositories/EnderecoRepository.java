1e22125353febb3417cc0cbd75ed9c3e0ff6d2d1
RobsonDev <robson.arcoleze29@gmail.com>
2023-01-04 13:33:44 -0300

    Criado EnderecoRepository

---
+++ backend/src/main/java/com/attornatus/testePratico/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.attornatus.testePratico.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.attornatus.testePratico.entities.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
+
+}
