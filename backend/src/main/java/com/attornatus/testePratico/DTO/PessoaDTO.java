8ba2e9b1a6f46a535916a3edae62f8fbe8e9db55
RobsonDev <robson.arcoleze29@gmail.com>
2023-01-04 00:19:18 -0300

    Implementado Construtor com sobrecarga de Lista de endereços

--- backend/src/main/java/com/attornatus/testePratico/DTO/PessoaDTO.java
+++ backend/src/main/java/com/attornatus/testePratico/DTO/PessoaDTO.java
@@ -1,7 +1,10 @@
 package com.attornatus.testePratico.DTO;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.attornatus.testePratico.entities.Endereco;
 import com.attornatus.testePratico.entities.Pessoa;
 
 public class PessoaDTO {
@@ -9,6 +12,8 @@ public class PessoaDTO {
 	private Long id;
 	private String name;
 	private LocalDate dataDeNascimento;
+	
+	private List<EnderecoDTO> enderecos = new ArrayList<>();
 	
 	public PessoaDTO(Long id, String name, LocalDate dataDeNascimento) {
 		this.id = id;
@@ -21,7 +26,12 @@ public class PessoaDTO {
 		name = entity.getName();
 		dataDeNascimento = entity.getDataDeNascimento();
 	}
-
+	
+	public PessoaDTO(Pessoa entity, List<Endereco> endereco) {
+		this(entity);
+		endereco.forEach(end -> this.enderecos.add(new EnderecoDTO(end)));
+	}
+	
 	public Long getId() {
 		return id;
 	}
@@ -32,6 +42,10 @@ public class PessoaDTO {
 
 	public LocalDate getDataDeNascimento() {
 		return dataDeNascimento;
+	}
+
+	public List<EnderecoDTO> getEnderecos() {
+		return enderecos;
 	}
 	
 	
